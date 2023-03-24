223a788507dc1b216f3ffae69f5efc260b1109d4
IdrisOmit <saddi.idris@gmail.com>
2023-03-24 01:05:01 +0100

    add some main component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './homePage/home/home.component';
+import { OutputPageComponent } from './outputPage/output-page/output-page.component';
+import { SubscriptionPageComponent } from './subscriptionPage/subscription-page/subscription-page.component';
+import { HomePageComponent } from './homePage/home-page/home-page.component';
+import { WaitingPageComponent } from './waitingPage/waiting-page/waiting-page.component';
+import { InputPageComponent } from './inputPage/input-page/input-page.component';
+import { LoginPageComponent } from './authenticationPages/login-page/login-page.component';
+import { SignupPageComponent } from './authenticationPages/signup-page/signup-page.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './globalComponent/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    OutputPageComponent,
+    SubscriptionPageComponent,
+    HomePageComponent,
+    WaitingPageComponent,
+    InputPageComponent,
+    LoginPageComponent,
+    SignupPageComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/inputPage/input-page/input-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-page',
+  templateUrl: './input-page.component.html',
+  styleUrls: ['./input-page.component.css']
+})
+export class InputPageComponent {
+
+}
