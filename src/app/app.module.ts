513f2d3231f2a1e983897f69004c0cb6006f296f
ossdet wxcql <ossama.ferjani@insat.ucar.tn>
2023-03-31 14:38:40 +0000

    navbar footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { OutputPageComponent } from './outputPage/output-page/output-page.component';
 import { SubscriptionPageComponent } from './subscriptionPage/subscription-page/subscription-page.component';
